f75b4efe3fc6d19a9244566374466da4c27f7b99
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-07-16 15:33:12 -0600

    added all components in file tree

---
+++ src/components/InputList.js
@@ -0,0 +1,47 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Input from "./Input";
+import { useSelector } from 'react-redux'
+import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase'
+
+function InputList(props) {
+
+  useFirestoreConnect([
+    { collection: 'inputs' }
+  ]);
+
+  const input = useSelector(state => state.firestore.ordered.inputs);
+
+  if (isLoaded(inputs)) {
+    return (
+      <React.Fragment>
+        <hr />
+        {input.map((input) => {
+          return <Input
+            whenInputClicked={props.onInputSelection}
+            name={input.name}
+            location={input.location}
+            event={input.event}
+            guests={input.guests}
+            date={input.date}
+          
+            formattedWaitTime={input.formattedWaitTime}
+            id={input.id}
+            key={input.id} />
+        })}
+      </React.Fragment>
+    )
+  } else {
+    return (
+      <React.Fragment>
+        <h3>Loading...</h3>
+      </React.Fragment>
+    )
+  }
+}
+
+InputList.propTypes = {
+  onInputSelection: PropTypes.func
+};
+
+export default InputList;
