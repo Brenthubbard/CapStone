bf29cdbc17499c41813f80ccd4db210c05c2169f
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-07-19 22:07:35 -0600

    went through every file to find out why it wasnt loading or linking to the data base, I never turned on the auth on the firestore it is now working and I can start to do some styling

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,18 @@
 // import * as firebase from 'firebase';
 import firebase from 'firebase/app'
 import 'firebase/firestore';
-import 'firebase/database';
-import 'firebase/auth';
+// import 'firebase/database';
+// import 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  measurment: process.env.REACT_APP_FIREBASE_APP_MEASUREMENT,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurment: process.env.REACT_APP_FIREBASE_APP_MEASUREMENT
 }
 
 firebase.initializeApp(firebaseConfig);
