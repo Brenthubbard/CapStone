ffe5125c0218c69948d094930e72edcbc74cc741
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-07-27 14:36:08 -0600

    Line 58:3:  Parsing error: 'import' and 'export' may only appear at the top level error

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+// import React from "react";
 import Header from "./Header/Header";
 import InputControl from "./InputControl";
 import Signin from "./SignIn";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NewInputForm from "./NewInputForm";
 import "./App.css"
+import React, { useState } from "react"
 
 function App() {
-  // const [fname, setFname] = useState("")
-
+  const [state, setState] = useState({
+    fname: "",
+    lname: "",
+  })
+  const handleChange = e => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    })
   return (
     <Router>
       <Header />
@@ -19,6 +27,25 @@ function App() {
         <Route path="/">
           <InputControl />
         </Route>
+        <div>
+          <h1>React Form Handling</h1>
+          <form>
+            <label>
+              First Name:{" "} <input type="text" name="fname"
+                value={state.fname} onChange={handleChange} />
+            </label>
+            <label>
+              Last Name:{" "}
+              <input
+                type="text"
+                name="lname"
+                value={state.lname}
+                onChange={handleChange}
+              />
+            </label>
+          </form>
+          <h5>        Name: {state.fname} {state.lname}</h5>
+        </div>
           {/* <div className="App">
             <Header />
           </div> */}
@@ -28,4 +55,4 @@ function App() {
   );
 }
 
-export default App;
+  export default App;
